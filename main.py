5288bf39d2fce9f97396e94fe49fa9c3501817f3
Shih-Hao Chen <samuelshchen@gmail.com>
2022-02-19 12:08:39 +1100

    v1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 from src.io import get_answer_words, get_guesses_words
 from src.logic.wordle import Wordle
 from src.solver import Solver
@@ -9,15 +11,21 @@ def main():
     answer_words = get_answer_words()
     guesses_words = get_guesses_words()
 
-    for word in answer_words[:10]:
+    for word in tqdm(answer_words):
         wordle = Wordle(word)
         bot = Solver(wordle, answer_words, guesses_words)
         result = bot.solve()
 
         result_dict[result] = result_dict.get(result, 0) + 1
 
-    for result, freq in result_dict.items():
+    sum = 0
+    cnt = 0
+    for result, freq in sorted(result_dict.items()):
         print(f"In {result} guesses: {freq} times")
+        sum += result * freq
+        cnt += freq
+
+    print(f"EV: {sum / cnt:.2f}")
 
 
 if __name__ == "__main__":
--- src/solver.py
+++ src/solver.py
@@ -1,19 +1,51 @@
+from logging import Filter
 from typing import List
 
 from .logic.wordle import Wordle
 
 
 class Solver:
-    def __init__(self, wordle: Wordle, answer_words: List[str], guess_words: List[str]):
+    def __init__(
+        self, wordle: Wordle, answer_words: List[str], guesses_words: List[str]
+    ):
         self.wordle = wordle
         self.answer_words = answer_words
-        self.guess_words = guess_words
+        self.guesses_words = guesses_words
+
+        self.guesses = []
+        self.results = []
+
+    def possible_word(self, word: str) -> bool:
+        fake_wordle = Wordle(word)
+        for i in range(len(self.guesses)):
+            if fake_wordle.guess(self.guesses[i]) != self.results[i]:
+                return False
+        return True
+
+    def possible_answer_words(self) -> List[str]:
+        return filter(self.possible_word, self.answer_words)
+
+    def possible_guesses_words(self) -> List[str]:
+        return filter(self.possible_word, self.guesses_words)
+
+    def make_guess(self) -> str:
+        """Uses the current state and chooses the best word to guess next
+
+        Returns:
+            str: The chosen 5 letter word as the next best guess
+        """
+        return next(self.possible_answer_words())
 
     def solve(self) -> int:
         for num_guess in range(1, 100):
             # Make a guess
-            result = self.wordle.guess("")
-            if result == "GGGGG":
+            guess = self.make_guess()
+            result = self.wordle.guess(guess)
+
+            if result == "GGGGG":  # 5 Greens!
                 return num_guess
+
+            self.guesses.append(guess)
+            self.results.append(result)
 
         raise RuntimeError("Stupid")
