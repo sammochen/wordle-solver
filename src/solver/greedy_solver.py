dd2098fa44de6b55708d98384600614cf5f7197b
Shih-Hao Chen <samuelshchen@gmail.com>
2022-02-21 21:44:47 +1100

    add exhaustive search

--- src/solver/greedy_solver.py
+++ src/solver/greedy_solver.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 from overrides import overrides
 
@@ -12,19 +12,32 @@ answer_words = get_answer_words()
 guess_words = get_guess_words()
 
 
-def memoised_make_guess(results: List[str], answer_words_left: List[str], memo: Memo):
-    result_key = "".join(results)
-    if result_key in memo.memo:
-        return memo.memo[result_key]
+def split_answer_words(answer_words_left: List[str], guess: str):
+    result_dict = {}
+    for answer_word in answer_words_left:
+        result = calc_wordle(answer_word, guess)
+        if result not in result_dict:
+            result_dict[result] = []
+        result_dict[result].append(answer_word)
+    return result_dict
+
+
+def memoised_make_guess(
+    guesses: List[str], results: List[str], answer_words_left: List[str], memo: Memo
+) -> Tuple[float, str]:
+    assert len(guesses) == len(results)
+
+    memo_key = ",".join(guesses) + "+" + ",".join(results)
+    if memo_key in memo.memo:
+        tup = tuple(memo.memo[memo_key])
+        return tup
 
     if len(answer_words_left) == 1:
-        memo.memo[result_key] = answer_words_left[0]
-        return answer_words_left[0]
-
-    best_guess = ""
-    lowest_expected_num_words_left = 1e9  # We want to lower this!!
+        memo.memo[memo_key] = (1, answer_words_left[0])
+        return (1, answer_words_left[0])
 
     # Hypothetical - what if we guessed "guess"
+    candidates = []
     for guess in guess_words:  # 10_000
         result_freq_dict = {}  # result -> List of answers that it could be
         for possible_answer_word in answer_words_left:  # 2_000
@@ -40,16 +53,39 @@ def memoised_make_guess(results: List[str], answer_words_left: List[str], memo: 
                 sum += result_freq
             cnt += 1
 
-        expected_num_words_left = sum / cnt
-        if expected_num_words_left <= lowest_expected_num_words_left:
-            lowest_expected_num_words_left = expected_num_words_left
-            best_guess = guess
+        expected_group_size = sum / cnt
+        candidates.append((expected_group_size, guess))
+
+    candidates.sort(key=lambda x: x[0])
+
+    num_candidates = 3
+    best_candidates = candidates[:num_candidates]
+    assert len(best_candidates) == 3
+
+    # Now that we have our best k guesses, we can actually try them
+    best_ev, best_guess = (1e9, "")
+    for _, candidate in best_candidates:
+        ev = 1  # The cost to make one guess
+
+        result_dict = split_answer_words(answer_words_left, candidate)
+        for result, answer_words in result_dict.items():
+            if result == "GGGGG":
+                # No more cost
+                continue
+
+            next_ev, _ = memoised_make_guess(
+                guesses + [candidate], results + [result], answer_words, memo
+            )
+            ev += next_ev * (len(answer_words) / len(answer_words_left))
+
+        if ev < best_ev:
+            best_ev, best_guess = ev, candidate
 
     if len(answer_words_left) == len(answer_words):
         print(f"root: {best_guess}")
 
-    memo.memo[result_key] = best_guess  # memoise
-    return best_guess
+    memo.memo[memo_key] = best_ev, best_guess  # memoise
+    return best_ev, best_guess
 
 
 class GreedySolver(Solver):
@@ -70,7 +106,9 @@ class GreedySolver(Solver):
             num_guesses += 1
 
             # Make a guess
-            guess = memoised_make_guess(self.results, self.word_filter.words, self.memo)
+            ev, guess = memoised_make_guess(
+                self.guesses, self.results, self.word_filter.words, self.memo
+            )
             result = self.wordle.guess(guess)
 
             self.guesses.append(guess)
